caa231ec7d5ab6e8ff10cfd56ad86616f6d8226c
Phillip Kim <phillipkim156@gmail.com>
2026-05-03 08:09:16 +1200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Phillip Kim on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
