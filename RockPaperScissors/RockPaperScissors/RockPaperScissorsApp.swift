3b9f97c6c4bf0c9242cd5bdb280db5ecc6573dad
Phillip Kim <phillipkim156@gmail.com>
2026-05-30 09:43:47 +1200

    100 Days of SwiftUI day 25

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Phillip Kim on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
