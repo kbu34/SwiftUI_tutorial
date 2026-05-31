64daeada8f633fc94170f72d855179882cb03ef3
Phillip Kim <phillipkim156@gmail.com>
2026-05-31 15:23:31 +1200

    100 Days of SwiftUI Day 36

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Phillip Kim on 31/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var
+    
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Phillip Kim on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
