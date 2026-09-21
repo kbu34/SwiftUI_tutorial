0f577e5313d47a63e5c7b7cca2732fd28440fe0a
Phillip Kim <phillipkim156@gmail.com>
2026-09-21 22:41:39 +1200

    100 Days of SwiftUI Day 40 Using generics to load any kind of Codable data

---
+++ MoonShot/MoonShot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  MoonShot
+//
+//  Created by Phillip Kim on 21/09/2026.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
