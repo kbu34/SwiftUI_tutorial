b2bb253c525398cdfc8c239492d80828a855eb53
Phillip Kim <phillipkim156@gmail.com>
2026-09-21 22:22:55 +1200

    100 Days of SwiftUI Day 40 Loading a specific kind of codable data

---
+++ MoonShot/MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Phillip Kim on 21/09/2026.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
